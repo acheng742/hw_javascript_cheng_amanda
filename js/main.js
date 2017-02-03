85d1860c36b07eab561504fa8ca002d219ce8f33
Amanda <acheng742@gmail.com>
2017-02-03 17:11:28 -0500

    adds info of past 5 president's in an array

---
+++ js/main.js
@@ -0,0 +1,11 @@
+var iceCream = ["Strawberry", "Butter Pecan", "Coffee"];
+iceCream.push("Cookie Dough");
+
+var presidents = [
+  {firstName: "Donald", lastName: "Trump", termLength: "1", party: "Republican", yearsOfPresidency: "0"},
+  {firstName: "Barack", lastName: "Obama", termLength: "2", party: "Democrat", yearsOfPresidency: "8"},
+  {firstName: "George W.", lastName: "Bush", termLength: "2", party: "Republican", yearsOfPresidency: "8"},
+  {firstName: "Bill", lastName: "Clinton", termLength: "1", party: "Democrat", yearsOfPresidency: "4"},
+  {firstName: "George H. W.", lastName: "Bush", termLength: "1", party: "Republican", yearsOfPresidency: "4"}
+];
+console.log(presidents[2].firstName + presidents[2].lastName);
